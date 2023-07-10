24cc9d1c4f37db2877de00fc3b713f7557719cef
shreyajmera99 <ajmerashrey83@gmail.com>
2023-07-11 03:47:08 +0530

    all chnges

--- src/App.js
+++ src/App.js
@@ -1,63 +1,41 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import News from './components/News';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-    loading: true,
+      loading: true, // Set initial loading state to true
     };
   }
 
   componentDidMount() {
     // Simulating API loading for 3 seconds
     setTimeout(() => {
-    this.setState({ loading: false });
+      this.setState({ loading: false });
     }, 3000);
   }
 
   render() {
-    const { loading } = this.state;
 
     return (
-      <div>
-        <Router basename="/newsapp">
-  
-          <Navbar />
-          <Routes>
-            <Route
-              path="/"
-              element={<News key="general" country="in" category="general" />}
-            />
-            <Route
-              path="/business"
-              element={<News key="business" country="in" category="business" />}
-            />
-            <Route
-              path="/entertainment"
-              element={<News key="entertainment" country="in" category="entertainment" />}
-            />
-            <Route
-              path="/health"
-              element={<News key="health" country="in" category="health" />}
-            />
-            <Route
-              path="/science"
-              element={<News key="science" country="in" category="science" />}
-            />
-            <Route
-              path="/technology"
-              element={<News key="technology" country="in" category="technology" />}
-            />
-            <Route
-              path="/sports"
-              element={<News key="sports" country="in" category="sports" />}
-            />
-          </Routes>
-        </Router>
-      </div>
+      <>
+        <div>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<News key="general" country="in" category="general" />} />
+              <Route path="/business" element={<News key="business" country="in" category="business" />} />
+              <Route path="/entertainment" element={<News key="entertainment" country="in" category="entertainment" />} />
+              <Route path="/health" element={<News key="health" country="in" category="health" />} />
+              <Route path="/science" element={<News key="science" country="in" category="science" />} />
+              <Route path="/technology" element={<News key="technology" country="in" category="technology" />} />
+              <Route path="/sports" element={<News key="sports" country="in" category="sports" />} />
+            </Routes>
+          </Router>
+        </div>
+      </>
     );
   }
 }
